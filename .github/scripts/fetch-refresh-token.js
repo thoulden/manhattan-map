386cc9792c0950fe3fa6c026c27a6b11cbcf6b3c
thoulden <101445737+thoulden@users.noreply.github.com>
2025-09-15 00:20:29 +1000

    Update fetch-refresh-token.js

--- .github/scripts/fetch-refresh-token.js
+++ .github/scripts/fetch-refresh-token.js
@@ -1,4 +1,7 @@
 // No imports needed on Node 20
+// access via: https://www.strava.com/oauth/authorize?client_id=CLIENT_ID&response_type=code&redirect_uri=https%3A%2F%2Fruns.thomas-houlden.com%2Fstrava-callback.html&scope=read,activity:read_all&approval_prompt=force
+
+
 
 (async () => {
   const { STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET, STRAVA_CODE } = process.env;
